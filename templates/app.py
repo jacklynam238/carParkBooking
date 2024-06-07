c3408d8b3b783417816920e34511d1c4f4ff71a6
Keira Murray <keira.murray@ericsson.com>
2024-06-07 11:32:21 +0100

    .

---
+++ templates/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+USERNAME = 'root'
+PASSWORD = ''
+HOST = 'localhost'
+DB_NAME = 'list_db'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + USERNAME + ':' + PASSWORD + '@' + HOST + '/' + DB_NAME
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    __tablename__ = 'list_table'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        item_name = request.form['item']
+        item = Item(name=item_name)
+        db.session.add(item)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    items = Item.query.all()
+    return render_template('index.html', items=items)
+
+
+@app.route('/delete/<int:item_id>', methods=['POST'])
+def delete_item(item_id):
+    item = Item.query.get_or_404(item_id)
+    db.session.delete(item)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/update/<int:item_id>', methods=['POST'])
+def update_item(item_id):
+    new_item = request.form['new_item']
+    item = Item.query.get_or_404(item_id)
+    item.name = new_item
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
