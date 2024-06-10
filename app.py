a025ee3039d9ff788d92fbd0371cf6611adb9b6f
Jack Lynam <jack.lynam@ericsson.com>
2024-06-10 16:48:48 +0100

    changed signup page

--- app.py
+++ app.py
@@ -7,10 +7,9 @@ HOST = 'localhost'
 DB_NAME = 'booking_db'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + USERNAME + ':' + PASSWORD + '@' + HOST + '/' + DB_NAME
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://'+USERNAME+':'+PASSWORD+'@'+HOST+'/'+DB_NAME
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 
 class Item(db.Model):
     __tablename__ = 'booking_table'
@@ -21,12 +20,12 @@ class Item(db.Model):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # if request.method == 'POST':
-    #     item_nam = request.form['item']
-    #     item = Item(name=item_name)
-    #     db.session.add(item)
-    #     db.session.commit()
-    #     return redirect(url_for('index'))
+    if request.method == 'POST':
+        item_name = request.form['item']
+        item = Item(name=item_name)
+        db.session.add(item)
+        db.session.commit()
+        return redirect(url_for('index'))
 
     items = Item.query.all()
     return render_template('index.html', items=items)
